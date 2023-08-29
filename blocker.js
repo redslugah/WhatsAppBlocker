5519f9d462c74ed0a6c19b42ff70d9252b80f81f
redslugah <rafael.douglas.1@gmail.com>
2023-08-28 23:19:17 -0300

    GFix done, Grid WIP

--- blocker.js
+++ blocker.js
@@ -1,7 +1,7 @@
 let defaultNumbers = ["+55 32 8865-0690"];
 let forbiddenLabels = [
   "Talvez Gui",
-  "Talvez Guilherme Augusto"
+  "Talvez Guilherme Augusto", "~Gui", "~Guilherme Augusto"
 ];
 
 function hideMessagesWithLabel(label) {
@@ -17,27 +17,42 @@ function hideMessagesWithNumber(number) {
   document
     .querySelectorAll(`[data-pre-plain-text*="${number}:"]`)
     .forEach((element) => {
-      const container = element.closest('div[data-testid="msg-container"]');
+      const container = element.closest('div[class="CzM4m"]');
       if (container && !!container.style) {
         container.style.display = "none";
       }
     });
 }
 
-function hideMessagesQuoted(number) {
+function hideMessagesQuoted(lumber) {
   document
-    .querySelectorAll(`[data-pre-plain-text*="${number}:"]`)
+    .querySelectorAll(`[data-pre-plain-text*="${lumber}:"]`)
     .forEach((element) => {
-      const container = element.closest('div[aria-label="Mensagem citada"]');
+      const container = element.closest('div[class="CzM4m"]');
       if (container && !!container.style) {
         container.style.display = "none";
       }
     });
 }
+
+//WIP
+/*function hideMessageGridWithNumber(lumber) {
+  document
+    .querySelectorAll(`[aria-label="${lumber}"]`)
+    .forEach((element) => {
+      const container = element.closest('div[class="_2KKXC"]');
+      console.log('newstuff2')
+      if (container && !!container.style) {
+        container.style.display = "none";
+      }
+    });
+}*/
 
 setInterval(() => {
+  //defaultNumbers.forEach((number) => hideMessageGridWithNumber(number));
   defaultNumbers.forEach((number) => hideMessagesWithNumber(number));
   defaultNumbers.forEach((number) => hideMessagesQuoted(number));
+  //forbiddenLabels.forEach((label) => hideMessageGridWithNumber(label));
   forbiddenLabels.forEach((label) => hideMessagesWithLabel(label));
   forbiddenLabels.forEach((label) => hideMessagesQuoted(label));
 }, 250);
